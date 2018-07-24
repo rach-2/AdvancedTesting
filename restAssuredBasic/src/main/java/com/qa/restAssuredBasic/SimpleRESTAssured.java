ae5bef924332623dec63eb5410e84748d910bbac
rach-2 <rache.lowe@btconnect.com>
2018-07-24 07:55:27 +0100

    worked more on intermate excise

--- restAssuredBasic/src/main/java/com/qa/restAssuredBasic/SimpleRESTAssured.java
+++ restAssuredBasic/src/main/java/com/qa/restAssuredBasic/SimpleRESTAssured.java
@@ -6,7 +6,6 @@ import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import io.restassured.response.ValidatableResponse;
 import io.restassured.specification.RequestSpecification;
-
 import static io.restassured.RestAssured.*;
 import static io.restassured.matcher.RestAssuredMatchers.*;
 import static org.hamcrest.Matchers.*;
